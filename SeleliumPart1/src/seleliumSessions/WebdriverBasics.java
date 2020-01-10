8745d89ece018f2cb18c5e00dabf90f9228b6593
Mausami14 <mausami.dhara132@gmail.com>
2020-01-10 17:56:56 +0530

    1st commit

---
+++ SeleliumPart1/src/seleliumSessions/WebdriverBasics.java
@@ -0,0 +1,23 @@
+package seleliumSessions;
+
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebdriverBasics {
+
+	public static void main(String[] args) {
+		
+	
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\md00619071\\Documents\\selelium jar\\chromedriver_win32\\chromedriver.exc");
+		WebDriver driver= new ChromeDriver();
+		
+		
+		
+		
+
+
+
+	}
+
+}
